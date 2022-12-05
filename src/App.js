9f8c4cb8740699369a5cfcd8701c701d6f96424b
amin <aminbabaei82@gmail.com>
2022-12-05 14:24:10 +0330

    add font - icon and completed navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-center font-bold underline">
-      Hello world!
-    </h1>
+   <Navbar/>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+          main: ["iransans"],
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({
+      addComponents
+    }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '680px',
+          },
+          '@screen md': {
+            maxWidth: '992px',
+          },
+          '@screen xl': {
+            maxWidth: '1200px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1450px',
+          },
+        }
+      })
+    },
+  ],
 }
