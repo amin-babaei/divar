84c6108acc5238541521ae115c76642b65525e57
amin <aminbabaei82@gmail.com>
2022-12-06 11:45:58 +0330

    added react router dom and fix structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import AppContainers from "./containers/AppContainers"
+import Layout from "./containers/Layout"
+import Support from "./pages/Support"
+import NotFound from "./pages/NotFound"
+
+const App = () => {
+  return (
+    <Routes>
+    <Route path="/" element={<AppContainers />} />
+    <Route element={<Layout/>}>
+      <Route path="/support" element={<Support/>} />
+    </Route>
+    <Route path="*" element={<NotFound/>} />
+  </Routes>
+  )
+}
+
+export default App
