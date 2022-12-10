28c9ab23b6e940b94f4818c184ea53544469a74d
amin <aminbabaei82@gmail.com>
2022-12-11 01:47:54 +0330

    fetching post

--- src/components/ads/AdsItem.jsx
+++ src/components/ads/AdsItem.jsx
@@ -1,32 +1,31 @@
-import { toPersianDigits } from '../../utils/persianDigit'
+import {toPersianDigits} from '../../utils/persianDigit'
 import moment from 'jalali-moment'
-const AdsItem = () => {
+
+const AdsItem = ({data}) => {
+  
     return (
         <>
-            <div className="border gap-2 flex justify-between border-gray-200 rounded px-2 py-4">
-                <div className="flex flex-col h-full justify-between md:w-1/2">
-                    <h5 className='text-md break-words font-semibold'>گوشی سامسونگ</h5>
-                    <div>
-                        <h5 className='text-sm pb-4'>{toPersianDigits('200,000')} تومان</h5>
-                        <p className='text-xs text-gray-500'>{moment("2022-12-08T22:54:53.702+00:00").locale('fa').fromNow()}</p>
+            {data?.docs.map(post => (
+                <article  key={post._id} className="border gap-2 flex justify-between border-gray-200 rounded px-2 py-4">
+                    <div className="flex flex-col h-full justify-between md:w-1/2">
+                        <h5 className='text-md break-words font-semibold'>{post.title}</h5>
+                        <div>
+                            <h5 className='text-sm pb-4'>
+                                {toPersianDigits(new Intl.NumberFormat('en-US', {
+                                    style: 'currency',
+                                    currency: 'IRR'
+                                }).format(post.price).replace("IRR", ""))}
+                                <span className='pr-1'>تومان</span>
+                            </h5>
+                            <p className='text-xs text-gray-500'>{moment(post.createdAt).locale('fa').fromNow()}</p>
+                        </div>
                     </div>
-                </div>
-                <div className='relative overflow-hidden rounded-md w-36 h-36 bg-gray-300 aspect-w-16 aspect-h-9 aspect-none'>
-                    <img src="/test3.jpg" className="absolute w-full h-full object-center" alt="test" />
-                </div>
-            </div>
-            <div className="border gap-2 flex justify-between border-gray-200 rounded px-2 py-4">
-                <div className="flex flex-col h-full justify-between md:w-1/2">
-                    <h5 className='text-md break-words font-semibold'>موتور هندا مدل 1394</h5>
-                    <div>
-                        <h5 className='text-sm pb-4'>{toPersianDigits('200,000')} تومان</h5>
-                        <p className='text-xs text-gray-500'>{moment("2022-12-07T22:54:53.702+00:00").locale('fa').fromNow()}</p>
+                    <div
+                        className='relative overflow-hidden rounded-md w-36 h-36 bg-gray-300 aspect-w-16 aspect-h-9 aspect-none'>
+                        <img src={`${process.env.REACT_APP_BASE_URL}/${post.image}`} className="absolute w-full h-full object-center" alt="test"/>
                     </div>
-                </div>
-                <div className='relative overflow-hidden rounded-md w-36 h-36 bg-gray-300 aspect-w-16 aspect-h-9 aspect-none'>
-                    <img src="/test1.webp" className="absolute w-full h-full object-center" alt="test" />
-                </div>
-            </div>
+                </article>
+            ))}
         </>
     )
 }
--- src/containers/AdsContainer.jsx
+++ src/containers/AdsContainer.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
 import AdsItem from '../components/ads/AdsItem'
+import useFetch from "../hooks/useFetch.js";
+import {toast} from "react-toastify";
+import Loading from "../components/Loading";
 
 const AdsContainer = () => {
+  const {data, loading, error} = useFetch(`${process.env.REACT_APP_BASE_URL}/api/posts`)
+  if (loading) return <Loading/>
+  if (error) {
+      toast.error(error.message)
+  }
   return (
     <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3 mt-12 md:mt-28 sm:mr-72">
-          <AdsItem/>
+          <AdsItem data={data}/>
     </div>
   )
 }
