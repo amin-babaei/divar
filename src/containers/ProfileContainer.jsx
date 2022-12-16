d0c650998fe26edf3c01d15b097b1acd2766337a
amin <aminbabaei82@gmail.com>
2022-12-16 13:13:24 +0330

    protect route

--- src/containers/ProfileContainer.jsx
+++ src/containers/ProfileContainer.jsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 import ProfileSidebar from "../pages/profile/ProfileSidebar"
 
 const ProfileContainer = () => {
+  const {user} = useAuth()
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if(!user)navigate('/signin')
+  },[user])
   return (
-    <section className="container mx-auto px-3 relative font-Ilight">
+    <section className={`container mx-auto px-3 relative font-Ilight ${user ? 'opacity-100' : 'opacity-0'}`}>
         <ProfileSidebar/>
         <Outlet/>
     </section>
