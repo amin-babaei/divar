6839e3b1d5c60c356af4017d25ab1887155c960f
amin <aminbabaei82@gmail.com>
2022-12-17 21:13:50 +0330

    add refetchOnWindowFocus

--- src/hooks/fetchData.js
+++ src/hooks/fetchData.js
@@ -10,17 +10,25 @@ import {
 } from "../services/fetchData"
 
 export const useAllCategorys = () => {
-    return useQuery("category", getCategorys)
+    return useQuery("category", getCategorys,{
+        refetchOnWindowFocus:false
+    })
 }
 export const usePosts = (currentPage, category, sort) => {
-    return useQuery(['posts', currentPage, category, sort], () => getPosts(currentPage, category, sort))
+    return useQuery(['posts', currentPage, category, sort], () => getPosts(currentPage, category, sort),{
+        refetchOnWindowFocus:false
+    })
 }
 export const usePost = (slug, hashId) => {
     return useQuery(['post', slug, hashId], () => getPost(slug, hashId))
 }
 export const useBookmarks = () => {
-    return useQuery('bookmarks', getBookmarks)
+    return useQuery('bookmarks', getBookmarks,{
+        refetchOnWindowFocus:false
+    })
 }
 export const useMyPosts = () => {
-    return useQuery('my-posts', getMyPosts)
+    return useQuery('my-posts', getMyPosts,{
+        refetchOnWindowFocus:false
+    })
 }
