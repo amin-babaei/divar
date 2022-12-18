ed349adb140a624bf8514104cbf715a31f7c0cdc
amin <aminbabaei82@gmail.com>
2022-12-18 03:50:53 +0330

    change bookmark handled with react query

--- src/pages/posts/Bookmark.jsx
+++ src/pages/posts/Bookmark.jsx
@@ -1,35 +1,18 @@
-import { useEffect, useState } from "react"
 import { BsBookmark, BsBookmarkFill } from "react-icons/bs"
-import { toast } from "react-toastify"
 import { useAuth } from '../../context/AuthContext'
-import http from "../../services/httpService"
+import { useBookmarked } from "../../hooks/fetchData"
 
 const Bookmark = ({ postId, bookmarked }) => {
     const { user } = useAuth()
-    const [active, setActive] = useState(bookmarked)
+    const mutation = useBookmarked()
 
-    useEffect(() => {
-        setActive(bookmarked)
-    }, [bookmarked])
-    
-    const handleBookmark = async () => {
-        try {
-            const res = await http.put(`/api/posts/bookmark/${postId}`)
-            if (res.status === 200) {
-                toast.success(res?.data?.message)
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error?.response?.data?.message)
-        }
+    const handleBookmark = async (postId) => {
+        if (user)mutation.mutate(postId)
     }
-    const handleClick = (id) => {
-        if (user) setActive(prevActiv => !prevActiv)
-        handleBookmark(id)
-    }
+
     return (
-        <button className='rounded-full duration-500 hover:bg-gray-200 p-3' onClick={() => handleClick(postId)}>
-            {active ? <BsBookmarkFill className="text-blue-500" /> : <BsBookmark className="text-gray-500" />}
+        <button className='rounded-full duration-500 hover:bg-gray-200 p-3' onClick={() => handleBookmark(postId)}>
+            {bookmarked ? <BsBookmarkFill className="text-blue-500" /> : <BsBookmark className="text-gray-500" />}
         </button>
     )
 }
