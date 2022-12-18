515fb741d62555e113d92c1af2bb9db8ae95c776
amin <aminbabaei82@gmail.com>
2022-12-18 03:47:34 +0330

    add option refetchOnWindowFocus

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import AuthProvider from './context/AuthContext';
 import { QueryProvider } from './context/QueryContext';
 import { QueryClient, QueryClientProvider } from 'react-query';
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
