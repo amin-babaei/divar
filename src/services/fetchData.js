9977f4b7dab5deb4b452b9ce3680c92ca0d1dc5e
amin <aminbabaei82@gmail.com>
2022-12-18 03:50:06 +0330

    added fetcher functions and fix urls

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,26 +1,30 @@
 import http from './httpService'
 
 export const getCategorys = async () => {
-    const {data} = await http.get(`${process.env.REACT_APP_BASE_API_URL}/api/post-category`)
+    const {data} = await http.get(`/api/post-category`)
     return data.data
 }
-
 export const getPosts = async (currentPage,category,sort) => {
-    const {data} = await http.get(`${process.env.REACT_APP_BASE_API_URL}/api/posts?page=${currentPage}&category=${category}&sort=${sort}`)
+    const {data} = await http.get(`/api/posts?page=${currentPage}&category=${category}&sort=${sort}`)
     return data.data
 }
-
 export const getPost = async (slug,hashId) => {
-    const {data} = await http.get(`${process.env.REACT_APP_BASE_API_URL}/api/posts/${slug}/${hashId}`)
+    const {data} = await http.get(`/api/posts/${slug}/${hashId}`)
     return data.data
 }
-
-export const getBookmarks = async () => {
-    const {data} = await http.get(`${process.env.REACT_APP_BASE_API_URL}/api/posts/bookmarks`)
-    return data.data
+export const createPost = async (data) => {
+    const response = await http.post(`/api/posts/create`,data)
+    return response
 }
-
 export const getMyPosts = async () => {
-    const {data} = await http.get(`${process.env.REACT_APP_BASE_API_URL}/api/posts/my-posts`)
+    const {data} = await http.get(`/api/posts/my-posts`)
     return data.data
+}
+export const getBookmarks = async () => {
+    const {data} = await http.get(`/api/posts/bookmarks`)
+    return data.data
+}
+export const editBookmark = async (postId) => {
+    const response = await http.put(`/api/posts/bookmark/${postId}`)
+    return response
 }
