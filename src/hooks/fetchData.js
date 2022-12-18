cf0658df0a0162b15c83eec1a9dd48503afaa25e
amin <aminbabaei82@gmail.com>
2022-12-18 03:49:05 +0330

    created custom hook mutation for bookmarks and my-posts

--- src/hooks/fetchData.js
+++ src/hooks/fetchData.js
@@ -1,13 +1,5 @@
-import {
-    useQuery
-} from "react-query"
-import {
-    getBookmarks,
-    getCategorys,
-    getMyPosts,
-    getPost,
-    getPosts
-} from "../services/fetchData"
+import {useMutation,useQuery,useQueryClient} from "react-query"
+import {createPost,editBookmark,getBookmarks,getCategorys,getMyPosts,getPost,getPosts} from "../services/fetchData"
 
 export const useAllCategorys = () => {
     return useQuery("category", getCategorys)
@@ -18,9 +10,25 @@ export const usePosts = (currentPage, category, sort) => {
 export const usePost = (slug, hashId) => {
     return useQuery(['post', slug, hashId], () => getPost(slug, hashId))
 }
-export const useBookmarks = () => {
-    return useQuery('bookmarks', getBookmarks)
+export const useCreatePost = () => {
+    const queryClient = useQueryClient()
+    return useMutation(createPost,{
+        onSuccess: () => {
+            queryClient.invalidateQueries('my-posts')
+        },
+    })
 }
 export const useMyPosts = () => {
     return useQuery('my-posts', getMyPosts)
+}
+export const useBookmarks = () => {
+    return useQuery('bookmarks', getBookmarks)
+}
+export const useBookmarked = () => {
+    const queryClient = useQueryClient()
+    return useMutation(editBookmark,{
+        onSuccess: () => {
+            queryClient.invalidateQueries('post')
+        },
+    })
 }
