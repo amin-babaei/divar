f9fe23a72cab28dc281b60c7079bff2b91c3899a
amin <aminbabaei82@gmail.com>
2022-12-21 02:06:02 +0330

    add react google recaptcha

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,46 +1,61 @@
-import { Button } from 'flowbite-react'
 import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
-import {validationSignup} from '../utils/validation.js'
+import { validationSignup } from '../utils/validation.js'
 import { useAuth, useAuthActions } from '../context/AuthContext';
-
+import ReCAPTCHA from "react-google-recaptcha";
 import Input from '../components/Input'
 import Loading from '../components/Loading'
+
 const initialValues = {
-    name:'',
-    email:'',
-    password:'',
-    phoneNumber:'',
-    confirmPassword:''
+    name: '',
+    email: '',
+    password: '',
+    phoneNumber: '',
+    confirmPassword: '',
+    token: ''
 }
 const Signup = () => {
     const dispatch = useAuthActions()
     const { loading } = useAuth()
-    const onSubmit = (values) => {
-        const {name,email,password,phoneNumber} = values
-        dispatch({type:'SIGNUP',payload:{name,email,password,phoneNumber}})
-        
-      }
+    const onSubmit = async (values) => {
+        const { name, email, password, phoneNumber, token } = values
+        dispatch({ type: 'SIGNUP', payload: { name, email, password, phoneNumber, token } })
+    }
+
     const formik = useFormik({
         initialValues,
         onSubmit,
-        validationSchema:validationSignup,
-        validateOnMount:true
+        validationSchema: validationSignup,
+        validateOnMount: true
     })
-    if(loading)return <Loading/>
+
+    if (loading) return <Loading />
+
     return (
-        <form className="flex flex-col gap-4 px-3 md:w-1/3 md:mx-auto mt-10 font-Imedium" onSubmit={formik.handleSubmit}>
+        <form className="flex flex-col gap-4 px-3 md:w-1/3 md:mx-auto mt-8 font-Imedium" onSubmit={formik.handleSubmit}>
             <h3 className='text-center text-gray-500 mb-5'>
                 ثبت نام
             </h3>
-                <Input lable='نام و نام خانوادگی' name='name' placeholder='امین بابایی' formik={formik}/>
-                <Input lable='ایمیل' name='email' type='email' placeholder='aminbabaei_dev@yahoo.com' formik={formik}/>
-                <Input lable='شماره تلفن' name='phoneNumber' placeholder='09935679611' formik={formik}/>
-                <Input lable='کلمه عبور' name='password' type='password' placeholder='****' formik={formik}/>
-                <Input lable='تکرار کلمه عبور' name='confirmPassword' type='password' placeholder='****' formik={formik}/>
-            <Button type="submit" color="failure">
-                ورود به حساب کاربری
-            </Button>
+            <Input lable='نام و نام خانوادگی' name='name' placeholder='امین بابایی' formik={formik} />
+            <Input lable='ایمیل' name='email' type='email' placeholder='aminbabaei_dev@yahoo.com' formik={formik} />
+            <Input lable='شماره تلفن' name='phoneNumber' placeholder='09935679611' formik={formik} />
+            <Input lable='کلمه عبور' name='password' type='password' placeholder='****' formik={formik} />
+            <Input lable='تکرار کلمه عبور' name='confirmPassword' type='password' placeholder='****' formik={formik} />
+            <ReCAPTCHA
+                sitekey={process.env.REACT_APP_RECAPTCHA_SITEKEY}
+                hl='fa'
+                onChange={value => {
+                    formik.setFieldValue("token", value)
+                }}
+            />
+            {formik.errors.token && formik.touched.token && (
+                <lable className='text-red-500 text-xs'>
+                    {formik.errors.token}
+                </lable>
+            )}
+            <button type="submit" disabled={!(formik.isValid)} className={`py-2 w-full rounded text-white ${(formik.isValid) ? 'bg-red-700 hover:hover:opacity-80' : 'bg-gray-500'}`}>
+                ثبت نام
+            </button>
             <Link to='/signin' className='text-sm underline cursor-pointer w-fit'>
                 قبلا ثبت نام کردید ؟ وارد شوید
             </Link>
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -11,7 +11,8 @@ export const validationSignup = Yup.object({
     email:Yup.string().required('ایمیل الزامی است').email('ایمیل نامعتبر است'),
     password:Yup.string().required('کلمه عبور الزامی است').min(8, 'کلمه عبور حداقل 8 کاراکتر باشد'),
     phoneNumber:Yup.string().required('شماره تلفن الزامی است').matches(/09([0-9])-?[0-9]{4}-?[0-9]{4}/,'شماره تلفن اشتباه است'),
-    confirmPassword:Yup.string().oneOf([Yup.ref('password'),""],'کلمه عبور همخوانی ندارد').required('تکرار کلمه عبور الزامی است')
+    confirmPassword:Yup.string().oneOf([Yup.ref('password'),""],'کلمه عبور همخوانی ندارد').required('تکرار کلمه عبور الزامی است'),
+    token:Yup.string().required("شما ربات هستید !").nullable()
 })
 export const validationCreatePost = Yup.object({
     title:Yup.string().required('عنوان آگهی را وارد کنید'),
