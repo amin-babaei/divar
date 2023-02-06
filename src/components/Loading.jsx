dddd9ce78bf7f159a8f1a660ba58674c25f3a54c
amin <aminbabaei82@gmail.com>
2023-02-06 06:27:24 +0330

    fix height loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 import { Spinner } from 'flowbite-react'
 const Loading = () => {
     return ( 
-        <div className="min-h-screen flex flex-col items-center justify-center gap-y-3">
+        <div className="min-h-[90vh] flex flex-col items-center justify-center gap-y-3">
             <Spinner color="gray" />
             <p className="text-gray-500 text-sm font-Ilight">درحال دریافت اطلاعات</p>
         </div>
