04dfd7f076ca55bfb537c748505b916e2d69fe19
amin <aminbabaei82@gmail.com>
2023-06-04 15:01:41 +0330

    rendered drawer in mobile size

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,29 @@ import {GrLocation} from 'react-icons/gr'
 import { Link } from 'react-router-dom'
 import Drawer from './Drawer'
 import DropDown from './DropDown'
+import { useEffect, useState } from 'react'
 
 const Navbar = () => {
-    
+    const [isMobile, setMobile] = useState(false);
+
+    useEffect(() => {
+      if (window.innerWidth < 640) {
+        setMobile(true);
+      } else {
+        setMobile(false);
+      }
+  
+      const updateMedia = () => {
+        if (window.innerWidth < 640) {
+            setMobile(true);
+        } else {
+            setMobile(false);
+        }
+      };
+      window.addEventListener('resize', updateMedia);
+      return () => window.removeEventListener('resize', updateMedia);
+    }, []);
+
   return (
     <header className='bg-white shadow-sm w-full sticky top-0 font-Imedium py-2 z-10'>
         <nav className='flex items-center justify-between container mx-auto px-3'>
@@ -13,7 +33,7 @@ const Navbar = () => {
                     <img src="/download.svg" alt="logo" className='w-12 h-12 cursor-pointer'/>
                 </Link>
                 <hr className='inline-block h-5 w-[1px] bg-gray-200 mx-3'/>
-                <Drawer/>
+                {isMobile && <Drawer/>}
                 <button className='hidden rounded px-4 py-[10px] text-gray-400 text-sm sm:flex items-center hover:bg-gray-100 hover:text-black'>
                     <GrLocation className='ml-1 text-lg'/>
                     مشهد
