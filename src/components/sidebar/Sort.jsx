4983ed9ec11f83d840f1d6796519c53e254bcccf
amin <aminbabaei82@gmail.com>
2023-07-29 19:42:43 +0330

    fix : add checked attribute to sort input

--- src/components/sidebar/Sort.jsx
+++ src/components/sidebar/Sort.jsx
@@ -21,11 +21,11 @@ const SidebarSort = () => {
           </button>
             <div className={`flex flex-col mb-1 font-light overflow-hidden transition-all duration-500 ease-out ${showDropdown ? "max-h-20 opacity-100" : "max-h-0 opacity-0"}`}>
              <label className="inline-flex items-center">
-                <input type="radio" className={`checked:bg-none focus:ring-0 focus:ring-offset-0 ${sort === 'desc' ? 'text-red-700 bg-red-700' : 'text-white checked:border-gray-500'}`} name="accountType" value="desc" onChange={handleChange}/>
+                <input type="radio" className={`checked:bg-none focus:ring-0 focus:ring-offset-0 ${sort === 'desc' ? 'text-red-700 bg-red-700' : 'text-white checked:border-gray-500'}`} name="accountType" value="desc" checked={sort === 'desc'} onChange={handleChange}/>
                 <span className="text-sm mr-2">بالاترین قیمت</span>
               </label>
               <label className="inline-flex items-center mt-4">
-                <input type="radio" className={`checked:bg-none focus:ring-0 focus:ring-offset-0 ${sort === 'asc' ? 'text-red-700 bg-red-700' : 'text-white checked:border-gray-500'}`} name="accountType" value="asc" onChange={handleChange}/>
+                <input type="radio" className={`checked:bg-none focus:ring-0 focus:ring-offset-0 ${sort === 'asc' ? 'text-red-700 bg-red-700' : 'text-white checked:border-gray-500'}`} name="accountType" value="asc" checked={sort === 'asc'} onChange={handleChange}/>
                 <span className="text-sm mr-2">کمترین قیمت</span>
               </label>
             </div>
