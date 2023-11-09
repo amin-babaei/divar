72ef9c6e0864b72ac5e7ee1fb6286dd70f6d2988
amin <aminbabaei82@gmail.com>
2023-11-09 18:43:42 +0330

    style: improve width label tag for better ux

--- src/components/sidebar/Sort.jsx
+++ src/components/sidebar/Sort.jsx
@@ -16,12 +16,12 @@ const SidebarSort = () => {
         <IoIosArrowDown className={`ml-4 duration-300 ${showDropdown && 'rotate-180'}`} />
       </button>
       <div className={`flex flex-col mb-1 font-light overflow-hidden transition-all duration-500 ease-out ${showDropdown ? "max-h-20 opacity-100" : "max-h-0 opacity-0"}`}>
-        <label className="inline-flex items-center">
+        <label className="inline-flex items-center max-w-fit">
           <input type="radio" className={`checked:bg-none focus:ring-0 focus:ring-offset-0 ${sort === 'desc' ? 'text-red-700 bg-red-700' : 'text-white checked:border-gray-500'}`} name="accountType" value="desc" checked={sort === 'desc'}
             onChange={(e) => sortQuery(setSearchParams, e.target.value)} />
           <span className="text-sm mr-2">بالاترین قیمت</span>
         </label>
-        <label className="inline-flex items-center mt-4">
+        <label className="inline-flex items-center mt-4 max-w-fit">
           <input type="radio" className={`checked:bg-none focus:ring-0 focus:ring-offset-0 ${sort === 'asc' ? 'text-red-700 bg-red-700' : 'text-white checked:border-gray-500'}`} name="accountType" value="asc" checked={sort === 'asc'}
             onChange={(e) => sortQuery(setSearchParams, e.target.value)} />
           <span className="text-sm mr-2">کمترین قیمت</span>
