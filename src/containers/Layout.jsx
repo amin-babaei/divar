1b85f49c809af5dcd67d21e3706a3495c59697cc
amin <aminbabaei82@gmail.com>
2023-11-12 21:37:31 +0330

    refactor: move context to layout

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/navbar/Navbar'
+import Compose from '../context/store'
+import AuthProvider from '../context/AuthContext'
+import { ChatProvider } from '../context/ChatContext'
 
 const Layout = () => {
   return (
-    <>
+    <Compose components={[AuthProvider, ChatProvider]}>
         <Navbar/>
         <Outlet/>
-    </>
+    </Compose>
   )
 }
 
